f2095a66d452abf0d999879b0add0bf1fdc1603b
杨晓龙的号 <>
2022-08-12 18:47:20 +0800

    '1'

--- dist/script.js
+++ dist/script.js
@@ -1,20 +1,17 @@
-const width = window.innerWidth / 2;
+const width = window.innerWidth;
 const height = window.innerHeight;
 class createMatter {
   Engine = Matter.Engine;
   Render = Matter.Render;
   Runner = Matter.Runner;
   Bodies = Matter.Bodies;
+  Body = Matter.Body;
   Composite = Matter.Composite;
+  Composites = Matter.Composites;
+  Constraint = Matter.Constraint;
   bodys = [];
   constructor() {
-    const {
-      Engine,
-      Render,
-      Runner,
-      Bodies,
-      Composite
-    } = this;
+    const { Engine, Render, Runner, Bodies, Composite } = this;
     // create an engine
     this.engine = Engine.create();
 
@@ -27,30 +24,87 @@ class createMatter {
         height,
       },
     });
-    let ground = Bodies.rectangle(400, 910, 810, 60, {
-      isStatic: true
+    let ground = Bodies.rectangle(width / 2, height, width, 30, {
+      isStatic: true,
     });
+    let groundA = Bodies.rectangle(0, height / 2, 30, height, {
+      isStatic: true,
+    });
+    let groundB = Bodies.rectangle(width, height / 2, 30, height, {
+      isStatic: true,
+    });
+    // let groundC = Bodies.rectangle(width / 2, -30, width, 60, {
+    //   isStatic: true,
+    // });
+    var group = this.Body.nextGroup(true);
+
+    var ropeB = this.Composites.stack(350, 0, 5, 1, 10, 10, (x, y, index) => {
+      return this.Bodies.circle(x, y, 0.1, {
+        collisionFilter: { group: group },
+        isStatic: index === 0 ? true : false,
+        frictionAir: 0.05,
+        density: 0.1,
+        mass: 0.0001,
+      });
+    });
+    this.Composites.chain(ropeB, 0.5, 0, -0.5, 0, {
+      stiffness: 0.8,
+      length: 100,
+      render: { type: "line" },
+    });
+    this.Composite.add(
+      ropeB,
+      this.Constraint.create({
+        bodyB: ropeB.bodies[0],
+        pointB: { x: -20, y: 0 },
+        pointA: {
+          x: ropeB.bodies[0].position.x,
+          y: ropeB.bodies[0].position.y,
+        },
+        stiffness: 0.5,
+      })
+    );
+    let change = 5;
+    let changeX = ropeB.bodies[0].position.x;
+    setInterval(() => {
+      // ropeB.bodies[0].position.x =
+      //   Math.random() > 0.5
+      //     ? changeX + Math.random() * 200
+      //     : changeX - Math.random() * 200;
+      ropeB.bodies[0].position.x += change;
+      if (ropeB.bodies[0].position.x - changeX > 100) {
+        change = -10;
+      }
+      if (ropeB.bodies[0].position.x - changeX < -100) {
+        change = 10;
+      }
+    }, 16);
+
+    Composite.add(this.engine.world, [ropeB]);
 
     // add all of the bodies to the world
-    this.composite = Composite.add(this.engine.world, ground);
+    this.composite = Composite.add(this.engine.world, [
+      ground,
+      groundA,
+      groundB,
+      // groundC,
+    ]);
 
     // run the renderer
     Render.run(this.render);
 
-    // create runner
+    // // create runner vb
     this.runner = Runner.create({});
 
-    // run the engine
+    // // run the engine
     Runner.run(this.runner, this.engine);
   }
 
   createIrregular(x, y, points, options = {}) {
     if (!Array.isArray(points) || points.length < 3) return;
-    x += this.render.options.width / 2;
-    y += this.render.options.height / 2;
-    const vertexSets = [
-      []
-    ];
+    x += width / 2;
+    y += height / 2;
+    const vertexSets = [[]];
     for (let i = 0; i < points.length; i += 3) {
       vertexSets[0].push({
         x: -points[i],
@@ -62,7 +116,7 @@ class createMatter {
     // console.log("vertexSets: ", vertexSets);
     const irregular = this.Bodies.fromVertices(x, y, vertexSets, {
       angle: Math.PI,
-      ...options
+      ...options,
     });
     this.bodys.push(irregular);
     this.Composite.add(this.engine.world, irregular);
@@ -70,9 +124,9 @@ class createMatter {
 
   getCenterForThree(body) {
     return {
-      x: body.position.x - this.render.options.width / 2,
-      y: body.position.y - this.render.options.height / 2
-    }
+      x: body.position.x - width / 2,
+      y: body.position.y - height / 2,
+    };
   }
 }
 
@@ -91,7 +145,14 @@ class createThree {
 
     document.body.appendChild(this.renderer.domElement);
 
-    this.camera = new THREE.OrthographicCamera(width / -2, width / 2, height / 2, height / -2, 1, 1000);
+    this.camera = new THREE.OrthographicCamera(
+      width / -2,
+      width / 2,
+      height / 2,
+      height / -2,
+      1,
+      1000
+    );
     this.camera.position.x = 0;
     this.camera.position.y = 0;
     this.camera.position.z = 800;
@@ -103,7 +164,7 @@ class createThree {
   createCircle() {
     const geometry = new THREE.TorusKnotGeometry(100, 30, 100, 16);
     const material = new THREE.MeshBasicMaterial({
-      color: 0xffff00
+      color: 0xffff00,
     });
     const torusKnot = new THREE.Mesh(geometry, material);
     this.scene.add(torusKnot);
@@ -112,77 +173,79 @@ class createThree {
 }
 
 class contactThreeMatter {
-  three = new createThree();
+  // three = new createThree();
   matter = new createMatter();
   testGift = [];
   constructor() {
-    const circle = this.three.createCircle();
-    circle.geometry.rotateZ(Math.PI / 4)
-    this.matter.createIrregular(
-      circle.position.x,
-      circle.position.y,
-      [...circle.geometry.attributes.position.array], {
-        restitution: 0.8,
-        isStatic: true,
-      }
-    );
-    this.makeCircle()
-    this.render()
+    // const circle = this.three.createCircle();
+    // circle.geometry.rotateZ(Math.PI / 4);
+    // this.matter.createIrregular(
+    //   circle.position.x,
+    //   circle.position.y,
+    //   [...circle.geometry.attributes.position.array],
+    //   {
+    //     restitution: 0.8,
+    //     isStatic: true,
+    //   }
+    // );
+    // this.makeCircle();
+    // this.render();
     // this.render()
   }
-  render() {
-    let renderFlash = () => {
-      requestAnimationFrame(renderFlash); // 避免不必要得刷新
-      this.update()
-      this.three.renderer.render(this.three.scene, this.three.camera);
-    };
-    renderFlash()
-  }
-  update() {
-    this.makeCircle();
-    this.testGift = this.testGift.filter(({
-      three,
-      matter
-    }) => {
-      let bool = false;
-      if (!matter) return false;
-      if (matter.position.x >= -300 && matter.position.x <= width + 300 && matter.position.y >= -300 && matter.position.y <= height + 300) {
-        bool = true
-      } else {};
-      if (!bool) {
-        this.three.scene.remove(three)
-        this.matter.Composite.remove(this.matter.engine.world, matter);
-      };
-      return bool;
-    })
-    this.testGift.forEach(({
-      three,
-      matter
-    }) => {
-      const position = this.matter.getCenterForThree(matter);
-      three.position.x = position.x
-      three.position.y = -position.y
-    })
-  }
-  makeCircle() {
-    const geometry = new THREE.CircleGeometry(20, 32);
-    const material = new THREE.MeshBasicMaterial({
-      color: 0xffff00
-    });
-    const circle = new THREE.Mesh(geometry, material);
-    this.three.scene.add(circle);
-    const matterCircle = Matter.Bodies.circle(width / 2, -30, 20, {
-      restitution: 0.8
-    })
-    this.matter.Composite.add(this.matter.engine.world, matterCircle);
-    this.testGift.push({
-      three: circle,
-      matter: matterCircle
-    })
-  }
-  createTestGift() {
-
-  }
+  // render() {
+  //   let renderFlash = () => {
+  //     requestAnimationFrame(renderFlash); // 避免不必要得刷新
+  //     this.update();
+  //     this.three.renderer.render(this.three.scene, this.three.camera);
+  //   };
+  //   renderFlash();
+  // }
+  // update() {
+  //   this.makeCircle();
+  //   this.testGift = this.testGift.filter(({ three, matter }) => {
+  //     let bool = false;
+  //     if (!matter) return false;
+  //     if (
+  //       matter.position.x >= -300 &&
+  //       matter.position.x <= width + 300 &&
+  //       matter.position.y >= -300 &&
+  //       matter.position.y <= height + 300
+  //     ) {
+  //       bool = true;
+  //     } else {
+  //     }
+  //     if (!bool) {
+  //       this.three.scene.remove(three);
+  //       this.matter.Composite.remove(this.matter.engine.world, matter);
+  //     }
+  //     return bool;
+  //   });
+  //   this.testGift.forEach(({ three, matter }) => {
+  //     const position = this.matter.getCenterForThree(matter);
+  //     three.position.x = position.x;
+  //     three.position.y = -position.y;
+  //     three.rotateZ(matter.angle);
+  //   });
+  // }
+  // makeCircle() {
+  //   const geometry = new THREE.BoxGeometry(25, 25, 1);
+  //   const material = new THREE.MeshBasicMaterial({
+  //     color: 0xffff00,
+  //   });
+  //   const circle = new THREE.Mesh(geometry, material);
+  //   this.three.scene.add(circle);
+  //   const matterCircle = Matter.Bodies.circle(width / 2, -30, 20, {
+  //     restitution: 0.8,
+  //     friction: 1,
+  //     frictionAir: 0.005,
+  //   });
+  //   this.matter.Composite.add(this.matter.engine.world, matterCircle);
+  //   this.testGift.push({
+  //     three: circle,
+  //     matter: matterCircle,
+  //   });
+  // }
+  createTestGift() {}
 }
 
 new contactThreeMatter();
